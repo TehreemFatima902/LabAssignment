285b5983b627f903ec55e3a34d5ff94b2c30b528
Syed Wajeeh ul Hassan <wajeeh2019@namal.edu.pk>
2021-10-02 20:31:27 +0500

    Sub Functionality

---
+++ cal03.py
@@ -0,0 +1,11 @@
+#calculator in Python(v0.2)
+def sub(num1,num2):
+	return num1-num2
+
+def div(num1,num2):
+	return num1/num2
+
+def mul(num1, num2):
+	return num1 * num2
+# main
+print(sub(5,2),div(4,2),mul (6,4))
